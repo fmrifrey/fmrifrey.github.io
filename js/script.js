e2f2091ec05a416a941fa6257aafd2bc7413cb9f
fmrifrey <djfrey@umich.edu>
2026-04-04 13:55:41 -0400

    progress on home page/design, etc.

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Mobile Navigation Toggle
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
+
+// Close mobile menu when a link is clicked
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('show');
+    });
+});
